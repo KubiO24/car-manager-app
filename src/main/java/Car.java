a4c05026f6b5b9ce2008053539d5234a2457357d
KubiO24 <kubakowal2004@gmail.com>
2023-01-07 19:01:47 +0100

    Finish invoice generation

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -4,9 +4,8 @@ import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.UUID;
+import java.io.IOException;
+import java.util.*;
 
 public class Car {
     private Integer id;
@@ -36,10 +35,9 @@ public class Car {
         this.airbags.add(new Airbag("kanapa"));
         this.airbags.add(new Airbag("boczne"));
 
-        // create a big random number - maximum is ffffff (hex) = 16777215 (dez)
-        int nextInt = random.nextInt(0xffffff + 1);
-        // format it as hexadecimal string (with hashtag and leading zeros)
-        this.color = String.format("#%06x", nextInt);
+        String[] colors = {"#000000", "#808080", "#FF0000", "#008000", "#FFA500", "#0000FF"};
+        index = random.nextInt(colors.length);
+        this.color = colors[index];
 
         this.invoiceGenerated = false;
     }
@@ -70,14 +68,71 @@ public class Car {
         }
 
         document.open();
+
+        HashMap<String, BaseColor> colorMap = new HashMap<>() {{
+            put("black", BaseColor.BLACK);
+            put("gray", BaseColor.GRAY);
+            put("red", BaseColor.RED);
+            put("green", BaseColor.GREEN);
+            put("orange", BaseColor.ORANGE);
+            put("blue", BaseColor.BLUE);
+        }};
+
         Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
-        Chunk chunk = new Chunk("tekst", font); // akapit
+        Font bigFont = FontFactory.getFont(FontFactory.COURIER, 20, BaseColor.BLACK);
+        Font boldFont = FontFactory.getFont(FontFactory.COURIER, 16, Font.BOLD, BaseColor.BLACK);
+
+        String colorText = switch (this.color) {
+            case "#000000" -> "black";
+            case "#808080" -> "gray";
+            case "#FF0000" -> "red";
+            case "#008000" -> "green";
+            case "#FFA500" -> "orange";
+            case "#0000FF" -> "blue";
+            default -> "custom";
+        };
+
+        BaseColor baseColor;
+        if(colorText.equals("custom")) {
+            colorText = this.color;
+            int r = Integer.valueOf(this.color.substring(1, 3), 16);
+            int g = Integer.valueOf(this.color.substring(3, 5), 16);
+            int b = Integer.valueOf(this.color.substring(5, 7), 16);
+            baseColor = new BaseColor(r, g, b);
+        }else {
+            baseColor = colorMap.get(colorText);
+        }
+        Font coloredFont = FontFactory.getFont(FontFactory.COURIER, 16, baseColor);
+
+        Paragraph title = new Paragraph("INVOICE for: " + this.uuid, boldFont);
+        Paragraph model = new Paragraph("model: " + this.model, bigFont);
+        Paragraph color = new Paragraph("color: " + colorText, coloredFont);
+        Paragraph year = new Paragraph("year: " + this.year, font);
+        Paragraph airbag1 = new Paragraph("airbag: " + this.airbags.get(0).description + " - " + this.airbags.get(0).value, font);
+        Paragraph airbag2 = new Paragraph("airbag: " + this.airbags.get(1).description + " - " + this.airbags.get(1).value, font);
+        Paragraph airbag3 = new Paragraph("airbag: " + this.airbags.get(2).description + " - " + this.airbags.get(2).value, font);
+        Paragraph airbag4 = new Paragraph("airbag: " + this.airbags.get(3).description + " - " + this.airbags.get(3).value, font);
 
         try {
-            document.add(chunk);
-        } catch (DocumentException e) {
+            document.add(title);
+            document.add(model);
+            document.add(color);
+            document.add(year);
+            document.add(airbag1);
+            document.add(airbag2);
+            document.add(airbag3);
+            document.add(airbag4);
+            String[] models = {"BMW", "Fiat", "Mercedes", "Peugot"};
+            if(Arrays.asList(models).contains(this.model)) {
+                Image img = Image.getInstance("car brands images/" + this.model + ".png");
+                float scaler = ((document.getPageSize().getWidth() - document.leftMargin() - document.rightMargin()) / img.getWidth()) * 100;
+                img.scalePercent(scaler);
+                document.add(img);
+            }
+        } catch (DocumentException | IOException e) {
             throw new RuntimeException(e);
         }
+
         document.close();
         this.invoiceGenerated = true;
     }
